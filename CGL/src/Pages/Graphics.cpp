cc8fe6918f6009b3b8e7fe525babddf0341ef68c
xmevik <leonidp2@mail.ru>
2024-04-01 15:20:39 +0600

    Initial point for graphics

--- CGL/src/Pages/Graphics.cpp
+++ CGL/src/Pages/Graphics.cpp
@@ -1,6 +1,6 @@
 #include "headers/Graphics.h"
 
-Graphics::Graphics(HWND& mnHwnd, HINSTANCE& hInstance)
+Graphics::Graphics(HWND &mnHwnd, HINSTANCE &hInstance)
 {
 	this->mnWnd = mnHwnd;
 	this->hInstance = hInstance;
@@ -15,27 +15,27 @@ Graphics::Graphics(HWND& mnHwnd, HINSTANCE& hInstance)
 
 void Graphics::initNativeObj()
 {
-	WNDCLASSEXW graphicsClass{ sizeof(WNDCLASSEXW) };
+	WNDCLASSEXW GraphicsClass{ sizeof(WNDCLASSEXW) };
 
-	graphicsClass.cbClsExtra = 0;
-	graphicsClass.cbWndExtra = 0;
-	graphicsClass.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
-	graphicsClass.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	graphicsClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
-	graphicsClass.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
-	graphicsClass.hInstance = this->hInstance;
-	graphicsClass.lpfnWndProc = this->GraphicsProc;
-	graphicsClass.lpszClassName = this->ClassName.c_str();
-	graphicsClass.lpszMenuName = nullptr;
-	graphicsClass.style = 0;
+	GraphicsClass.cbClsExtra = 0;
+	GraphicsClass.cbWndExtra = 0;
+	GraphicsClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	GraphicsClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	GraphicsClass.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	GraphicsClass.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+	GraphicsClass.hInstance = this->hInstance;
+	GraphicsClass.lpfnWndProc = this->GraphicsProc;
+	GraphicsClass.lpszClassName = this->ClassName.c_str();
+	GraphicsClass.lpszMenuName = nullptr;
+	GraphicsClass.style = 0;
 
-	RegisterClassExW(&graphicsClass);
+	RegisterClassExW(&GraphicsClass);
 
-	this->GraphicsWnd = CreateWindowExW(0L, graphicsClass.lpszClassName, this->AppName.c_str(),
+	this->GraphicsWnd = CreateWindowExW(0L, GraphicsClass.lpszClassName, this->AppName.c_str(),
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		(GetSystemMetrics(SM_CXSCREEN) - this->widht) / 2u,
+		(GetSystemMetrics(SM_CXSCREEN) - this->width) / 2u,
 		(GetSystemMetrics(SM_CYSCREEN) - this->height) / 2u,
-		this->widht, this->height,
+		this->width, this->height,
 		this->mnWnd, nullptr, this->hInstance, this);
 
 	if (this->GraphicsWnd == INVALID_HANDLE_VALUE)
@@ -44,59 +44,115 @@ void Graphics::initNativeObj()
 
 void Graphics::createNativeControls()
 {
-	this->GoBackButton = CreateWindowExW(0l, L"button", L"Вернуться назад", WS_VISIBLE | WS_CHILD | ES_CENTER,
-		10, 10, 150, 25, this->GraphicsWnd,
-		reinterpret_cast<HMENU>(Graphics::PageInteraction::GoBackClicked), nullptr, nullptr);
+	this->hdc = GetDC(this->GraphicsWnd);
 }
 
 LRESULT CALLBACK Graphics::GraphicsProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	Graphics* hTable = reinterpret_cast<Graphics*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+	Graphics* hGraphics = reinterpret_cast<Graphics*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
 
-	if (hTable)
+	if (hGraphics)
 	{
-		hTable->GraphicsWnd = hwnd;
-		return hTable->windowProc(hwnd, uMsg, wParam, lParam);
+		hGraphics->GraphicsWnd = hwnd;
+		return hGraphics->windowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 LRESULT CALLBACK Graphics::windowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg) {
-		case WM_COMMAND:
+	switch (uMsg)
+	{
+		case WM_PAINT:
 		{
-			return this->handleCommand(hwnd, uMsg, wParam, lParam);
-		}
-		case WM_CLOSE:
-		{						// App::ButtonsInteraction::DestroyClicked
-			SendMessage(this->mnWnd, WM_COMMAND, LOWORD(1555), reinterpret_cast<LPARAM>(this->GraphicsWnd));
+			PAINTSTRUCT ps;
+			this->hdc = BeginPaint(hwnd, &ps);
+			this->mnPen = CreatePen(PS_SOLID, 2, this->color::White);
+			this->firtsGraphPen = CreatePen(PS_SOLID, 2, this->color::Green);
+			this->secGraphPen = CreatePen(PS_SOLID, 2, this->color::Blue);
 
-			return TRUE;
+			SelectObject(this->hdc, mnPen);
+			this->DrawAxes();
+			this->DrawGraph();
+
+			DeleteObject(this->mnPen);
+			DeleteObject(this->firtsGraphPen);
+			DeleteObject(this->secGraphPen);
+			ReleaseDC(hwnd, this->hdc);
+			EndPaint(hwnd, &ps);
+			break;
 		}
+
+		case WM_SIZE:
+		{
+			this->width = LOWORD(lParam);
+			this->height = HIWORD(lParam);
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		}
+
+		case WM_CLOSE:
+		{								// App::ButtonsInteraction::DestroyClicked
+			SendMessageW(this->mnWnd, WM_COMMAND, LOWORD(1555), reinterpret_cast<LPARAM>(this->GraphicsWnd));
+			return EXIT_SUCCESS;
+			break;
+		}
+
+		case WM_DESTROY:
+		{
+			SendMessageW(this->mnWnd, WM_COMMAND, LOWORD(1555), reinterpret_cast<LPARAM>(this->GraphicsWnd));
+			return EXIT_SUCCESS;
+			break;
+		}
+
 		default:
+		{
 			return DefWindowProc(hwnd, uMsg, wParam, lParam);
 			break;
+		}
+	}
+
+	return TRUE;
+}
+
+void Graphics::DrawAxes() const {
+	MoveToEx(this->hdc, 0, this->height / 2, NULL); // Ось Y
+	LineTo(this->hdc, this->width, height / 2);
+
+	MoveToEx(this->hdc, this->width / 2, 0, NULL); // Ось X
+	LineTo(this->hdc, this->width / 2, this->height);
+
+	// Насечки на осях (упрощенно)
+	for (int i = 0; i <= this->width; i += 50) {
+		MoveToEx(this->hdc, i, this->height / 2 - 5, NULL);
+		LineTo(this->hdc, i, this->height / 2 + 5);
+	}
+
+	for (int i = 0; i <= this->height; i += 50) {
+		MoveToEx(this->hdc, this->width / 2 - 5, i, NULL);
+		LineTo(this->hdc, this->width / 2 + 5, i);
 	}
 }
 
-LRESULT CALLBACK Graphics::handleCommand(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (static_cast<Graphics::PageInteraction>(LOWORD(wParam)))
-	{
-		case Graphics::PageInteraction::GoBackClicked:
-		{						// App::ButtonsInteraction::DestroyClicked
-			SendMessage(this->mnWnd, WM_COMMAND, LOWORD(1555), reinterpret_cast<LPARAM>(this->GraphicsWnd));
+void Graphics::DrawGraph() {
+	if (this->graphData.empty()) return;
 
-			return TRUE;
-		}
-		default:
-			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	// Пример отрисовки одного графика (упрощенно)
+	for (size_t i = 0; i < this->graphData[0].size() - 1; ++i) {
+		int x1 = static_cast<int>(i * (this->width / this->graphData[0].size()));
+		int y1 = this->height / 2 - static_cast<int>(this->graphData[0][i] * 20); // Умножаем на масштаб
+
+		int x2 = static_cast<int>((i + 1) * (this->width / this->graphData[0].size()));
+		int y2 = this->height / 2 - static_cast<int>(this->graphData[0][i + 1] * 20); // Умножаем на масштаб
+
+		MoveToEx(this->hdc, x1, y1, NULL);
+		LineTo(this->hdc, x2, y2);
 	}
 }
 
 void Graphics::ShowHWND(int nCmdShow) const
 {
 	ShowWindow(this->GraphicsWnd, nCmdShow);
+
 	UpdateWindow(this->GraphicsWnd);
 }
--- CGL/src/Pages/headers/Graphics.h
+++ CGL/src/Pages/headers/Graphics.h
@@ -5,32 +5,40 @@
 #include <exception>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Graphics
 {
-	enum PageInteraction
+	enum color
 	{
-		GoBackClicked,
+		Blue = RGB(0, 0, 255),
+		Green = RGB(0, 255, 0),
+		White = RGB(255, 255, 255),
 	};
 
 	private:
 		const wstring ClassName{ L"GraphicsWndClass" }, AppName{ L"Графики функций"};
-		HWND mnWnd, GraphicsWnd, GoBackButton;
-		UINT widht{ 500 }, height{ 500 };
+		vector<vector<double>> graphData;
+		HDC hdc;
+		HPEN mnPen, firtsGraphPen, secGraphPen;
+		HWND mnWnd, GraphicsWnd;
+		int width{ 1001 }, height{ 563 };
 		HINSTANCE hInstance;
 
 	public:
-		explicit Graphics(HWND& mnWnd, HINSTANCE& hInstance);
+		explicit Graphics(HWND &mnWnd, HINSTANCE &hInstance);
 
 		void ShowHWND(int nCmdShow) const;
 
 	private:
 		static LRESULT CALLBACK GraphicsProc(HWND, UINT, WPARAM, LPARAM);
 		LRESULT CALLBACK windowProc(HWND, UINT, WPARAM, LPARAM);
-		LRESULT CALLBACK handleCommand(HWND, UINT, WPARAM, LPARAM);
 		void initNativeObj();
 		void createNativeControls();
+
+		void DrawAxes() const;
+		void DrawGraph();
 };
 
